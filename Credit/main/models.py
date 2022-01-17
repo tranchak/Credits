c58f13073cf40455636c3652b634b8d6f6ae138d
oleg <olegpustovalov220@gmail.com>
2022-01-17 21:33:18 +0300

    add model Bank, Credit

--- Credit/main/models.py
+++ Credit/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Bank(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Имя Банка')
+    capital_sum = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='Капитал банка')
+    credit_sum = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='Сумма кредита')
+    client = models.ManyToManyField('Client', verbose_name='Клиент')
+    credit_name = models.ForeignKey('Credit', on_delete=models.CASCADE, verbose_name='Наименование кредита')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Банк'
+        verbose_name_plural = 'Банки'
+
+
+class Credit(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование кредита')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Наименование кредита'
+        verbose_name_plural = 'Наименование кредита'
